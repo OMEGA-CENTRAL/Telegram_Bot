d3bc86eac02007bcde8cda27e5bfee500e0ed8bd
Helion <81987134+OMEGA-CENTRAL@users.noreply.github.com>
2023-04-30 10:07:49 +0300

    Update distributionAlgorithm.py

--- WorkBot/distributionAlgorithm.py
+++ WorkBot/distributionAlgorithm.py
@@ -6,23 +6,23 @@ def distribution(basedir, folderName):
     filenames = []
     amount = 0
 
-    for root, dirs, files in os.walk(basedir + r"\Storage\images\\" + folderName):  
+    for root, dirs, files in os.walk(basedir + r"/Storage/images/" + folderName):  
         for filename in files:
 
             amount += 1
             filename = filename.replace('.jpg','')
             filenames.append(filename)
 
-    for root, dirs, files in os.walk(basedir + r"\Storage\imagesForClients\\" + folderName):  
+    for root, dirs, files in os.walk(basedir + r"/Storage/imagesForClients/" + folderName):  
         for filename in files:
 
-            os.remove(basedir + r"\Storage\imagesForClients\\" + folderName + "\\" + filename)
+            os.remove(basedir + r"/Storage/imagesForClients/" + folderName + "/" + filename)
 
     listPosition = 0
 
     for i in range(int(len(filenames) / 4) + 1):  
 
-        savePath = (basedir + r"\\Storage\\imagesForClients\\" + folderName + "\\" + str(i + 1) + "-")  
+        savePath = (basedir + r"/Storage/imagesForClients/" + folderName + "/" + str(i + 1) + "-")  
                       
         if (amount >= 1):
 
@@ -78,8 +78,8 @@ def distribution(basedir, folderName):
         else:
             fourthNameImage = 'empty'
 
-        if (os.path.exists(basedir + "\\Storage\\imagesForClients\\" + folderName) != True):
-            os.mkdir(basedir + "\\Storage\\imagesForClients\\" + folderName)
+        if (os.path.exists(basedir + "/Storage/imagesForClients/" + folderName) != True):
+            os.mkdir(basedir + "/Storage/imagesForClients/" + folderName)
         
         savePath += ".jpg"
 
